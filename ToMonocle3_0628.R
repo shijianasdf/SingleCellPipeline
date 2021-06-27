c14a5ea0f971e69520d6768f23c5c3e59f5c02fa
Seoyeon <syk991116@korea.ac.kr>
2021-06-28 01:38:31 +0900

    Code for converting SeuratObject to Monocle cds for pseudotime trajectory analysis

---
+++ ToMonocle3_0628.R
@@ -0,0 +1,95 @@
+library(monocle3)
+library(dplyr)
+library(tidyverse)
+library(Seurat)
+
+sample = "chromium0XX"
+
+
+seuratobj = readRDS(paste("Data/SeuratRDS/", sample, "_PC10_res0.5.rds", sep = ""))
+
+ToMonocle3 <- function(seurat_object,
+                       scale_all = FALSE,
+                       assay = "SCT",
+                       reduction_for_projection = "PCA",
+                       UMAP_cluster_slot = NULL){
+  
+  if(scale_all){
+    message("Getting residuals for all Seurat genes in chosen assay slot and placing in scale.data")
+    seurat_genes <- rownames(seurat_object[[assay]])
+    remaining_genes <- setdiff(seurat_genes, rownames(seurat_object[[assay]]@scale.data))
+    if(assay == "SCT"){
+      seurat_object <- Seurat::GetResidual(seurat_object, features = remaining_genes, assay = assay, umi.assay = "RNA")
+    } else {
+      seurat_object <- Seurat::ScaleData(seurat_object, features = rownames(seurat_object[[assay]]))
+    }
+  }
+  
+  #We prep the seurat object by creating gene loadings for ALL genes in the Seurat scale.data slot. This is done to allow downstream monocle3 functions on gene_modules to work appropriately.
+  message("Projecting gene loadings for all Seurat genes in scale.data slot")
+  seurat_object <- Seurat::ProjectDim(seurat_object, reduction = reduction_for_projection, assay = assay)
+  
+  ##################
+  
+  message("Initializing CDS object")
+  
+  #Extract Seurat's log-transformed values
+  expression_matrix <- Seurat::GetAssayData(seurat_object, assay = assay, slot = "counts")
+  #Extract Seurat meta_data
+  meta_data <- seurat_object@meta.data
+  #Extract gene names from Seurat object SCT slot to make CDS
+  seurat_genes <- data.frame(gene_short_name = rownames(seurat_object[[assay]]),
+                             row.names = rownames(seurat_object[[assay]]))
+  new_cds <- monocle3::new_cell_data_set(expression_data = expression_matrix, cell_metadata = meta_data, gene_metadata = seurat_genes)
+  
+  ##################
+  
+  message("Making an SCE object from the Seurat object to facilitate transfer of information from SCE to CDS")
+  sce <- as.SingleCellExperiment(seurat_object, assay = assay)
+  message("Loading in all Seurat reductions (PCA, HARMONY, UMAP, etc.) into CDS")
+  SingleCellExperiment::reducedDims(new_cds) <- SingleCellExperiment::reducedDims(sce)
+  message("Loading in specified Seurat assay into CDS")
+  SummarizedExperiment::assays(new_cds) <- SummarizedExperiment::assays(sce)
+  message("Loading in Seurat gene names into CDS")
+  SummarizedExperiment::rowData(new_cds) <- SummarizedExperiment::rowData(sce)
+  SummarizedExperiment::rowData(new_cds)$gene_short_name <-  row.names(new_cds)
+  message("Loading in Seurat gene loadings into CDS")
+  new_cds@preprocess_aux$gene_loadings <- seurat_object@reductions[[reduction_for_projection]]@feature.loadings.projected
+  
+  ##################
+  
+  message("Get user specified selected clusters (or active idents) from Seurat and load into CDS")
+  if(is.null(UMAP_cluster_slot)){
+    list_cluster <- Idents(seurat_object)
+  } else {
+    Idents(seurat_object) <- UMAP_cluster_slot
+    list_cluster <- Idents(seurat_object)
+  }
+  new_cds@clusters[["UMAP"]]$clusters <- list_cluster
+  #The next two commands are run in order to allow "order_cells" to be run in monocle3
+  rownames(new_cds@principal_graph_aux[['UMAP']]$dp_mst) <- NULL
+  colnames(SingleCellExperiment::reducedDims(new_cds)[["UMAP"]]) <- NULL
+  
+  ##################
+  
+  message("Setting all cells as belonging to one partition (multiple partitions not supported yet)")
+  recreate_partition <- c(rep(1, length(new_cds@colData@rownames)))
+  names(recreate_partition) <- new_cds@colData@rownames
+  recreate_partition <- as.factor(recreate_partition)
+  new_cds@clusters[["UMAP"]]$partitions <- recreate_partition
+  
+  ##################
+  message("Done")
+  new_cds
+}
+
+cds = ToMonocle3(seuratobj, scale_all = TRUE, assay = "RNA", reduction_for_projection = "pca", UMAP_cluster_slot = NULL)
+
+cds <- learn_graph(cds)
+cds <- order_cells(cds, reduction_method = "UMAP")
+
+plot_cells(
+  cds = cds,
+  color_cells_by = "pseudotime",
+  show_trajectory_graph = TRUE
+)
